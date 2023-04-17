5d938643d9bbb94cefb4e0459c913e768a579357
GeraDes10 <pmz11_garcia@hotmail.com>
2023-04-17 13:23:53 -0600

    PrimeroPasosTailwindCss

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import './index.css';
+import "./index.css";
 
 function App() {
   return (
-    <div>
-      <p className="text-2xl">Hola Mundo</p>
+    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-cover bg-no-repeat bg-center">
+      <h1 className="uppercase">todo</h1>
+      <button>Luna</button>
     </div>
   );
 }
