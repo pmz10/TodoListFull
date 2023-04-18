6f471ce06a32c180c5c08f7702527b630c0436ab
GeraDes10 <pmz11_garcia@hotmail.com>
2023-04-18 00:05:20 -0600

    Estructura del Todo

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,44 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-cover bg-no-repeat bg-center">
-      <h1 className="uppercase">todo</h1>
-      <button>Luna</button>
+    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat ">
+      <header className="container mx-auto px-4">
+        <h1 className="uppercase">todo</h1>
+        <button>Luna</button>
+        <form>
+          <input type="text" placeholder="Crear un nuevo todo..." />
+        </form>
+      </header>
+      <main className="container mx-auto px-4">
+        <article>
+          <button>circulo</button>
+          <p>Complete oline Javascript curse with PMZ</p>
+          <button>Exit</button>
+        </article>
+        <article>
+          <button>circulo</button>
+          <p>Complete oline Javascript curse with PMZ</p>
+          <button>Exit</button>
+        </article>
+        <article>
+          <button>circulo</button>
+          <p>Complete oline Javascript curse with PMZ</p>
+          <button>Exit</button>
+        </article>
+
+        <section>
+          <span>5 Items left</span>
+          <button>Clear Complete</button>
+        </section>
+      </main>
+
+      <section className="px-auto container mx-auto">
+        <button>All</button>
+        <button>Active</button>
+        <button>Completed</button>
+      </section>
+
+      <p className="text-center">Drag and drop to reorder list</p>
     </div>
   );
 }
