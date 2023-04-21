3f09126979c4421ecb65cf07b27d7f590fce4299
GeraDes10 <pmz11_garcia@hotmail.com>
2023-04-20 22:42:20 -0600

    Estilizando los Spams

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,47 @@ import "./index.css";
 
 function App() {
   return (
-    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat ">
+    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat bg-gray-300 min-h-screen">
       <header className="container mx-auto px-4">
-        <h1 className="uppercase">todo</h1>
-        <button>Luna</button>
-        <form>
-          <input type="text" placeholder="Crear un nuevo todo..." />
+        <div className="flex justify-between">
+          <h1 className="text-3xl uppercase text-white font-semibold tracking-[0.3em]">
+            todo
+          </h1>
+          <button>Luna</button>
+        </div>
+        <form
+          className="flex items-center gap-4 overflow-hidden  
+        rounded-md bg-white py-4 px-4 mt-8"
+        >
+          <span className="inline-block h-5 w-5 rounded-full border-2"></span>
+          <input
+            type="text"
+            placeholder="Crear un nuevo todo..."
+            className="w-full outline-none text-gray-400"
+          />
         </form>
       </header>
-      <main className="container mx-auto px-4">
-        <article>
-          <button>circulo</button>
-          <p>Complete oline Javascript curse with PMZ</p>
-          <button>Exit</button>
-        </article>
-        <article>
-          <button>circulo</button>
-          <p>Complete oline Javascript curse with PMZ</p>
-          <button>Exit</button>
-        </article>
-        <article>
-          <button>circulo</button>
-          <p>Complete oline Javascript curse with PMZ</p>
-          <button>Exit</button>
-        </article>
+
+      <main className="container mx-auto px-4 mt-8">
+        <div className="bg-white rounded-md px-4">
+          <article>
+            <button className="inline-block h-5 w-5 rounded-full border-2"></button>
+            <p className="text-gray-600">
+              Complete oline Javascript curse with PMZ
+            </p>
+            <button>Exit</button>
+          </article>
+          <article>
+            <button>circulo</button>
+            <p>Complete oline Javascript curse with PMZ</p>
+            <button>Exit</button>
+          </article>
+          <article>
+            <button>circulo</button>
+            <p>Complete oline Javascript curse with PMZ</p>
+            <button>Exit</button>
+          </article>
+        </div>
 
         <section>
           <span>5 Items left</span>
@@ -39,7 +56,6 @@ function App() {
         <button>Active</button>
         <button>Completed</button>
       </section>
-
       <p className="text-center">Drag and drop to reorder list</p>
     </div>
   );
