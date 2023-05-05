d7d3b17e14a07abd2d86903ec3209bd332597c5e
GeraDes10 <pmz11_garcia@hotmail.com>
2023-05-04 20:22:52 -0600

    Botón de sol y luna, ademas de funcionalidad respecto al tema dark

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,29 @@
+import { useEffect, useState } from "react";
 import MoonIcon from "./Iconos/MoonIcon";
+import IconSun from "./Iconos/IconSun";
 
 const Header = () => {
+  const [darkMode, setDarkMode] = useState(true);
+
+  useEffect(() => {
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    }else {
+      document.documentElement.classList.remove("dark");
+    }
+
+  },[darkMode]);
+
   return (
     <header className="container mx-auto px-4 pt-8">
       <div className="flex justify-between">
         <h1 className="text-3xl uppercase text-white font-semibold tracking-[0.3em]">
           todo
         </h1>
-        <button>
-          <MoonIcon />
+        <button onClick={() => setDarkMode(!darkMode)}>
+          {
+            darkMode ? <IconSun /> : <MoonIcon />
+          }
         </button>
       </div>
     </header>
--- src/Utils/darkMode.js
+++ src/Utils/darkMode.js
@@ -1,20 +1,22 @@
 // On page load or when changing themes, best to add inline `head` to avoid FOUC
 
-if (
-  localStorage.theme === "dark" ||
-  (!("theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  document.documentElement.classList.add("dark");
-} else {
-  document.documentElement.classList.remove("dark");
-}
+//if (
+  //localStorage.theme === "dark" ||
+  //(!("theme" in localStorage) &&
+    //window.matchMedia("(prefers-color-scheme: dark)").matches)
+//) {
+  //document.documentElement.classList.add("dark");
+  //localStorage.theme = "dark";
+//} else {
+  //document.documentElement.classList.remove("dark");
+  //localStorage.theme = "light";
+//}
 
 //Whenever then user explicitly chooses light mode
-localStorage.theme = "light";
+//localStorage.theme = "light";
 
 //Whenever then user explicitly chooses light mode
-localStorage.theme = "dark";
+//localStorage.theme = "dark";
 
 //Whenever then user explicitly chooses light mode
-localStorage.theme = "theme";
+//localStorage.theme = "theme";
